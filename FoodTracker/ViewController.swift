cb78f16cebefb9fb336655254b2595bba88d8c4c
Jarvis <student@macbook-pro-0457-mark-mccutcheon.local>
2016-10-27 14:30:08 -0400

    Finished the connecting outlets/actions prep lesson. Nothing new so far, all just super easy review

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        mealNameLabel.text = "Default Text"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
