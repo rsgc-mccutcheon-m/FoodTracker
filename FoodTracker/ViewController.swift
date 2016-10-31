5a3035488b9c3bbc31e50e4d7750507d44267103
Jarvis <student@macbook-pro-0457-mark-mccutcheon.local>
2016-10-31 08:32:30 -0400

    Setup the UITextfield Delegate. Added two functions to handle keyboard activity and setting meal name to text field input

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Properties
     
     @IBOutlet weak var mealNameLabel: UILabel!
     
     @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //Hide Keyboard
+        textField.resignFirstResponder()
+    
+        return true
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
     
     
     // MARK: Actions
@@ -25,9 +41,12 @@ class ViewController: UIViewController {
     }
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   nameTextField.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
