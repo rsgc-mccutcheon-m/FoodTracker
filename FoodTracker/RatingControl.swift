81bbd8d732fbbcea28b58bc10df1ebfc05f33910
Jarvis <student@macbook-pro-0457-mark-mccutcheon.local>
2016-11-04 08:56:27 -0400

    Finished the override layoutsubviews function! yayyayya!

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -37,6 +37,18 @@ class RatingControl: UIView {
         }
     }
     
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        for (index, button) in ratingButtons.enumerated() {
+            
+            buttonFrame.origin.x = CGFloat(index * (44+5))
+            button.frame = buttonFrame
+            
+        }
+    
+    }
+    
     override public var intrinsicContentSize: CGSize {
         get {
          return CGSize(width: 240, height: 44)
